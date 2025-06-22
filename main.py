2491f93d0eab4f872a419d20374f59e64e7ebffd
NMD555 <nguyenminhdangvl@gmail.com>
2025-06-22 20:55:37 +0700

    thu giao dien dark

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from ui.main_window import MainWindow
+import qdarkstyle
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
+	app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
 	window = MainWindow()
 	app.setStyle("Fusion")  # Buộc dùng giao diện Fusion
 	window.resize(500,250)
